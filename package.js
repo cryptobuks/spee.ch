f1136d41e7ffbcd1edfa402314b14b036e1ab9c0
bill bittner <bittner.w@gmail.com>
2018-03-23 12:54:31 -0700

    created index.js build

---
+++ package.js
@@ -0,0 +1,9 @@
+module.exports = {
+  App       : require('./exports/app.js'),
+  Components: require('./exports/components.js'),
+  Containers: require('./exports/containers.js'),
+  Pages     : require('./exports/pages.js'),
+  Reducers  : require('./exports/reducers.js'),
+  Sagas     : require('./exports/sagas.js'),
+  Server    : require('./exports/server.js'),
+};
